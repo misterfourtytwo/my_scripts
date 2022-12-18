cacef132e0c5a4d5bff74d0e185a3dfc6c6525d1
Aliaksei Tratseuski <aliaksei.tratseuski@gmail.com>
2022-12-19 02:48:03 +0400

    script updated

--- flutter_switcher.sh
+++ flutter_switcher.sh
@@ -6,6 +6,12 @@ rm $FLUTTER
 
 if [[ "${current_path: -4}" == "1.22" ]]; then
       ln -sn ${FLUTTER}_rolling $FLUTTER;
-else 
+      echo "swithced to rolling"
+else
       ln -sn ${FLUTTER}_1.22 $FLUTTER;
+      echo "swithced to 1.22"
 fi
+# wipe dart cache
+rm -rf ~/.dartServer/.analysis-driver
+# regenerate packages cache
+flutter pub cache repair
